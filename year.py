322caf381591c0b6881cff0230fe7ed5501a065b
narmadha04-tech <narmadhavelusamy25@gmail.com>
2025-12-26 14:23:11 +0530

    Refactor date validation code for clarity

--- year.py
+++ year.py
@@ -1,3 +1,4 @@
+#check whether the given date is valid or not
 date=input()
 d,m,y=date.split("/")
 d=int(d)
@@ -7,3 +8,4 @@ if(d>0 and d<=31) and (m>0 and m<=12) and (y>=1900 and y<=9999):
     print("Valid")
 else:
     print("Invalid")
+
