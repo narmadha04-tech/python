1d9079cccd2a87800460760d13193750a619a6de
Narmadha V <narmadhavelusamy25@gmail.com>
2026-01-09 10:33:16 +0530

    Add files via upload

---
+++ twosums_l1.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+nums = list(map(int, input().split()))
+target = int(input())
+print(twoSum(nums, target))
